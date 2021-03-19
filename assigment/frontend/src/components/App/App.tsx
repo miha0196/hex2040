e3cfb81d941b30eac1e136c5d4dc6811bf7e5efe
Michael Khakhel <miha0196@mail.ru>
2021-03-19 18:12:32 +0300

    added grid layout

--- assigment/frontend/src/components/App/App.tsx
+++ assigment/frontend/src/components/App/App.tsx
@@ -1,44 +1,33 @@
 import React, { useEffect, useState } from 'react';
 
+import { GridLayout } from '../GridLayout';
 import { Point, Hex, Layout } from '../HexGrid';
 
 import './style.css'
 
-type cellsParams = {
+type CellsParams = {
   x: number,
   y: number,
   z: number,
   value: number,
 }
 
-const displayGrid = (hexes: cellsParams[]) => {
-  const displayHex = new Layout(Layout.flat, new Point(0, 0), new Point(35, -35));
-
-  return hexes.map((cellsParams: cellsParams) => {
-    const {x, y, z, value} = cellsParams;
-    const hex = new Hex(x, y, z);
-    console.log(hex, displayHex.hexToPixel(hex));
-    return { coords: displayHex.hexToPixel(hex), hex, value };
-  });
+type DisplayedCells = {
+  coords: Point,
+  hex: Hex,
+  value: number
 }
 
-const createGrid = (gridSize: number): cellsParams[]  => {
-  const hexes: Hex[] = [];
+const displayGrid = (hexes: CellsParams[]) => {
+  const point1 = new Point(0, 0);
+  const point2 = new Point(35, -35);
+  const displayHex = new Layout(Layout.flat, point2, point1);
 
-  for (let i = -gridSize; i <= gridSize; i++) {
-    for (let j = -gridSize; j <= gridSize; j++) {
-
-      if (i - j < gridSize + 1 && i - j > -(gridSize + 1) ) {
-
-        hexes.push(new Hex(i, -j, -i + j));
-      }
-    }
-  }
-
-  return hexes.map((hex: Hex): cellsParams => {
-    const {q, r, s} = hex;
-    return { x: q, y: r, z: s, value: 0 }
-  })
+  return hexes.map((cellsParams: CellsParams) => {
+    const {x, y, z, value} = cellsParams;
+    const hex = new Hex(x, y, z);
+    return { coords: displayHex.hexToPixel(hex), hex, value };
+  });
 }
 
 // const changeHexValue = (x: number, y: number, z: number, value: number) => {
@@ -50,48 +39,67 @@ const createGrid = (gridSize: number): cellsParams[]  => {
 // }
 
 const App = () => {
-  const [cellsParams, setCellsParams] = useState();
+  // const [cellsParams, setCellsParams] = useState(() => createEmptyGrid(1));
+  const [cellsParams, setCellsParams] = useState([]);
 
-  function fetchCellsParams() {
-    fetch('http://localhost:13337/2', {
+  function fetchCellsParams(postData: CellsParams[]) {
+    fetch('http://localhost:13337/3', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify([])
+      body: JSON.stringify(postData)
     })
       .then(res => res.json())
-      .then(data => setCellsParams(data))
+      .then(data => setCellsParams(prevState  => prevState.concat(data)))
   }
 
   useEffect(() => {
-    fetchCellsParams();
+    fetchCellsParams([]);
   }, [])
+
+  useEffect(() => {
+
+    const keyDownHandler = (e: KeyboardEvent) => {
+      if (e.keyCode === 13) {
+        fetchCellsParams(cellsParams)
+      }
+    }
+
+    window.addEventListener('keydown', keyDownHandler);
+
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler)
+    }
+  }, [cellsParams])
 
   return (
     <div className="grid">
-      {
-        displayGrid(createGrid(1)).map(({ coords, hex, value }: { coords: {x: number, y: number}, hex: Hex, value: number }, idx: number) => {
-          return (
-            <div 
-              key={idx}
-              className="hexagon active"
-              data-x={hex.q}
-              data-y={hex.r}
-              data-z={hex.s}
-              data-value={value}
-              style={{
-                position: "absolute",
-                top: `${coords.y}px`,
-                left: `${coords.x}px`}}>
-              <svg version="1.1" height="58" width="66">
-                <polygon points="65,29 49,57 17,57 1,29 17,1 49,1 65,29" fill="rgb(236, 228, 219)" stroke="#999" strokeWidth="2" />
-              </svg>
-              <span></span>
-            </div>
-          ) 
-        })
-      }
+      <GridLayout size={2} displayGrid={displayGrid}>
+        {
+          displayGrid(cellsParams).map(({ coords, hex, value }: DisplayedCells, idx: number) => {
+            return (
+              <div
+                key={idx}
+                className="hexagon active"
+                data-x={hex.q}
+                data-y={hex.r}
+                data-z={hex.s}
+                data-value={value}
+                style={{
+                  position: "absolute",
+                  top: `${coords.y}px`,
+                  left: `${coords.x}px`
+                }}>
+                <svg version="1.1" height="58" width="66">
+                  <polygon points="65,29 49,57 17,57 1,29 17,1 49,1 65,29" fill={value !== 0 ? 'rgb(236, 228, 219)' : 'white'} stroke="#999" strokeWidth="2" />
+                </svg>
+                <span>{value === 0 ? '' : value}</span>
+              </div>
+            )
+          })
+        }
+      </GridLayout>
     </div>
   )
 }
